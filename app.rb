0c292c0abf45af008b7c77074c1000dbb52b40ee
Max Jacobson <max@hardscrabble.net>
2016-05-03 12:00:34 -0400

    actually it does work for files with spaces

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'shellwords'
 
-filename = "data(1).csv"
+filename = "all the data(1).csv"
 
 # this works, but is potentially unsafe
 # (we mitigate the risk by escaping singlequotes sometimes, which is probably
@@ -8,11 +8,10 @@ filename = "data(1).csv"
 system "sh app.sh '#{filename}'"
 
 # this doesn't work because we escape the filename and then put it in quotes, so
-# the escape characters \\ are interpreted as literal slashes
+# the escape characters \\ are interpreted as literal slashes and the file with
+# those slashes cannot be found
 system "sh app.sh '#{Shellwords.escape(filename)}'"
 
 # this works and is, I think, the ideal form:
 system "sh app.sh #{Shellwords.escape(filename)}"
 # I like it because it leverages the ruby standard for escaping
-
-# note that none of them would work if the file had a space in the filename...
